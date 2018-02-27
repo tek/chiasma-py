6004f7925b067a77a2609c879b5fc74f86cff88b
Torsten Schmits <torstenschmits@gmail.com>
2018-02-27 21:00:41 +0100

    make `Ident` an ADT

--- chiasma/util/id.py
+++ chiasma/util/id.py
@@ -1,11 +1,12 @@
 import re
 from uuid import UUID
-from typing import Union
+from typing import TypeVar, Generic, Any, Type
 
-from amino import Dat
-
-from amino import Map, Maybe, __
+from amino import Map, Maybe, __, Dat
 from amino.util.numeric import parse_int
+from amino import ADT, Either, List
+from amino.json.decoder import Decoder, decode_json_type_json
+from amino.json.data import JsonError, Json
 
 
 class Key(Dat['Key']):
@@ -19,7 +20,35 @@ class Key(Dat['Key']):
         return super()._str_extra.cat(self.name)
 
 
-Ident = Union[str, UUID, Key]
+A = TypeVar('A')
+
+
+class Ident(Generic[A], ADT['Ident']):
+
+    def __init__(self, value: A) -> None:
+        self.value = value
+
+
+class StrIdent(Ident[str]):
+    pass
+
+
+class UUIDIdent(Ident[UUID]):
+    pass
+
+
+class KeyIdent(Ident[Key]):
+    pass
+
+
+class IdentDecoder(Decoder, tpe=Ident):
+
+    def decode(self, tpe: Type[Ident], data: Json) -> Either[JsonError, Ident[Any]]:
+        return (
+            decode_json_type_json(data, Key).lmap(List)
+            .accum_error_lift(decode_json_type_json, data, UUID)
+            .accum_error_lift(decode_json_type_json, data, str)
+        )
 
 
 def parse_id(value, rex, desc):
@@ -51,4 +80,4 @@ def parse_pane_id(value):
     return parse_id(value, _pane_id_re, 'pane')
 
 
-__all__ = ('parse_window_id', 'parse_pane_id', 'parse_id', 'Key', 'Ident')
+__all__ = ('Ident', 'Key', 'StrIdent', 'UUIDIdent', 'KeyIdent', 'parse_window_id', 'parse_pane_id', 'parse_id')
