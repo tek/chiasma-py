e8ed3b9776c29358642591a7f01dcfc971c2d3f5
Torsten Schmits <torstenschmits@gmail.com>
2018-03-14 19:46:23 +0100

    rename `TmuxSpec.proc` to `tmux_proc`

--- chiasma/test/tmux_spec.py
+++ chiasma/test/tmux_spec.py
@@ -19,21 +19,21 @@ class TmuxSpec(SpecBase):
         self.win_height = 120
 
     def setup(self) -> None:
-        self.proc = start_tmux(tmux_spec_socket, self.win_width, self.win_height, True)
+        self.tmux_proc = start_tmux(tmux_spec_socket, self.win_width, self.win_height, True)
         self.tmux = Tmux.cons(tmux_spec_socket)
         self._wait(1)
         cmd = TmuxIO.read('list-clients -F "#{client_name}"')
-        self.client = cmd.unsafe(self.tmux).head.get_or_fail('no clients')
+        self.tmux_client = cmd.unsafe(self.tmux).head.get_or_fail('no clients')
 
     def teardown(self) -> None:
-        self.proc.kill()
-        self.proc.wait()
+        self.tmux_proc.kill()
+        self.tmux_proc.wait()
         kill_server().result(self.tmux)
 
     def run(self, prog: TS[D, None], data: D) -> None:
         self._wait(1)
         r = prog.run(data).unsafe(self.tmux)
-        TmuxIO.write('display-panes', '-t', self.client).result(self.tmux)
+        TmuxIO.write('display-panes', '-t', self.tmux_client).result(self.tmux)
         self._wait(1)
         return r
 
